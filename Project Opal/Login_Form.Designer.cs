90a4073b830a243f886d52b5b50ce48594a2cd5d
David Bel <dbel.tbay@gmail.com>
2013-01-21 17:04:55 -0500

    login process started

--- Project Opal/Login_Form.Designer.cs
+++ Project Opal/Login_Form.Designer.cs
@@ -28,19 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtUsername.Location = new System.Drawing.Point(8, 8);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(264, 26);
+            this.txtUsername.TabIndex = 0;
+            this.txtUsername.Text = "Username";
+            this.txtUsername.Click += new System.EventHandler(this.txtUsername_Click);
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtPassword.Location = new System.Drawing.Point(8, 48);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(264, 26);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.Text = "Password";
+            this.txtPassword.Click += new System.EventHandler(this.txtPassword_Click);
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(200, 88);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // Login_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 121);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Login_Form";
             this.Text = "Login";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+
     }
 }
--- Project Opal/Login_Form.cs
+++ Project Opal/Login_Form.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace Project_Opal
 {
@@ -15,6 +16,77 @@ namespace Project_Opal
         public Login_Form()
         {
             InitializeComponent();
+        }
+
+        private void txtUsername_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text.Equals("Username"))
+            {
+                txtUsername.ForeColor = Color.Black;
+                txtUsername.Text = "";
+            }
+        }
+
+        private void txtPassword_Click(object sender, EventArgs e)
+        {
+            if (txtPassword.Text.Equals("Password"))
+            {
+                txtPassword.ForeColor = Color.Black;
+                txtPassword.Text = "";
+            }
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+            if (txtUsername.ForeColor != Color.Black) { txtUsername.ForeColor = Color.Black; }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            if (txtPassword.ForeColor != Color.Black) { txtPassword.ForeColor = Color.Black; }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection con = null;
+            SQLiteCommand cmd = null;
+
+            try
+            {
+                con = new SQLiteConnection(Database.CONNECTION_STRING);
+                con.Open();
+
+                string stm = String.Format("SELECT password FROM T_USER WHERE username = '{0}'", txtUsername.Text);
+
+                cmd = new SQLiteCommand(stm, con);
+                var rows = cmd.ExecuteScalar();
+
+                if (rows != null)
+                {
+                    string retrievedPassword = rows.ToString();
+
+                    if (txtPassword.Text.Equals(retrievedPassword))
+                    {
+                        // ACCESS GRANTED
+                    }
+                    else
+                    {
+                        // ACCESS DENIED
+                    }
+                }
+                else
+                {
+                    // USER DOESN'T EXIST
+                }
+            }
+            catch (SQLiteException ex)
+            {
+                Logger.Write(ex.ToString());
+            }
+            finally
+            {
+                Database.CloseAndDispose(con, cmd);
+            }
         }
     }
 }
