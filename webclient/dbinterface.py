d5016cee11c03e326ad8634a110e57b8922641f2
Gary Graham <garygrantgraham@gmail.com>
2013-03-17 14:11:03 -0400

    Fixed Python SQL vulnerabilities #13
    Changed all SQL commands to use Parametrized SQL, which creates an
    execution plan based on the initial statement, and execute only that. It
    ignores any extra commands injected through parameters. Furthermore
    modified small routing things including capitalization of some routes.

--- webclient/dbinterface.py
+++ webclient/dbinterface.py
@@ -16,23 +16,25 @@ class DB:
             #test
 
     def createUser(self, fn, ln,ad, sin, wage):
+        argumentTuple = (fn + " " + ln, ad, "test11", sin, wage)
         stm = '''INSERT INTO T_USER
                  (name, address,bank_account, sin, wage)
                  VALUES
-                 ('%s','%s','%s',%s,%s)
-              '''%(fn + " " + ln,ad,"test11",sin,wage)
+                 (?,?,?,?,?)
+              '''
         #todo write some code that cuts spaces out of SIN
         #todo write some code that allows real numbers in the html textbox for wage
-
-        writeCheck = self.executeSql(stm)
+        print(stm)
+        writeCheck = self.executeSql(stm, argumentTuple)
         print(writeCheck)
 
 
     def grabPW(self, username):
         print("Entering _> GrabPW: For user %s" %username)
-        stm = "SELECT password FROM T_CREDENTIALS WHERE username = '%s'" %username
+        argumentTuple = (username,)
+        stm = "SELECT password FROM T_CREDENTIALS WHERE username = ?"
 
-        hashedPW = self.executeSql(stm)
+        hashedPW = self.executeSql(stm, argumentTuple)
 
         if hashedPW:
             return hashedPW[0][0]
@@ -41,7 +43,7 @@ class DB:
 
     def grabUserList(self):
         print("Entering -> GrabUserList")
-        stm = "SELECT username, user_id from T_CREDENTIALS ORDER BY username ASC"
+        stm = "SELECT name, id from T_USER ORDER BY name ASC"
         userList = self.executeSql(stm)
 
         if userList:
@@ -50,31 +52,38 @@ class DB:
             return None
 
     def grabShiftCountByUserID(self,userID):
-        stm = "SELECT COUNT(*) FROM T_SHIFT WHERE employee_id = %s" %userID
-        shiftCount = self.executeSql(stm)
+        argumentTuple = (userID,)
+        stm = "SELECT COUNT(*) FROM T_SHIFT WHERE employee_id = ?"
+        shiftCount = self.executeSql(stm, argumentTuple)
         return shiftCount[0][0]
-        #This is super patchwork code, should be tightened with try/catch stuff.
+        #todo This is super patchwork code, should be tightened with try/catch stuff.
 
     def grabStartTimesByUserID(self, userID):
-        stm = "SELECT start_time FROM T_SHIFT WHERE employee_id = %s" %userID
-        shiftStartTimes = self.executeSql(stm)
+        argumentTuple = (userID,)
+        stm = "SELECT start_time FROM T_SHIFT WHERE employee_id = ?"
+        shiftStartTimes = self.executeSql(stm, argumentTuple)
 
         returnArray = [theStartTime[0] for theStartTime in shiftStartTimes]
         print(returnArray)
         return returnArray
 
     def grabMostRecentShiftByUserID(self,userID):
-        stm = "SELECT start_time, end_time FROM T_SHIFT WHERE employee_id = %s ORDER BY start_time DESC" %userID
-        shiftStartTimes = self.executeSql(stm)
+        argumentTuple = (userID,)
+        stm = "SELECT start_time, end_time FROM T_SHIFT WHERE employee_id = ? ORDER BY start_time DESC"
+        shiftStartTimes = self.executeSql(stm, argumentTuple)
         for item in shiftStartTimes:
             print(item)
         return shiftStartTimes
 
 
-    def executeSql(self, sql):
+    def executeSql(self, sql, argumentTuple = None):
         print("Entering-> executeSql()")
         cursor = self.grab_cursor()
-        cursor.execute(sql)
+        if argumentTuple is not None:
+            cursor.execute(sql, argumentTuple)
+        else:
+            cursor.execute(sql)
+        self.conn.commit()
         resultSet = cursor.fetchall()
 
         #Checking integrity and returning
