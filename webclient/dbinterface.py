bf407ef62e94d2cb4caa5bf7141a22f41330c876
Gary Graham <garygrantgraham@gmail.com>
2013-03-21 17:37:07 -0400

    added graballshiftsbyuseridbymonth

--- webclient/dbinterface.py
+++ webclient/dbinterface.py
@@ -1,7 +1,7 @@
 import re
 import sqlite3
 import constants
-
+import time
 
 class DB:
     def __init__(self):
@@ -87,6 +87,42 @@ class DB:
         stm = "SELECT start_time, end_time FROM T_SHIFT WHERE employee_id = ? ORDER BY start_time DESC"
         shiftStartTimes = self.executeSql(stm, argumentTuple)
         return shiftStartTimes
+
+    def grabAllShiftsByMonthByUserID(self, userID):
+        argumentTuple = (userID,)
+        stm = "SELECT start_time from T_SHIFT where employee_id = ?"
+        allShifts = self.executeSql(stm, argumentTuple)
+        returnDict = { 1 : 0, 2 : 0, 3 : 0, 4 : 0, 5 : 0, 6 : 0, 7 : 0, 8 : 0, 9 : 0, 10 : 0, 11 : 0, 12 : 0}
+        for item in allShifts:
+            print(item[0])
+            if '-01-' in item[0]:
+                returnDict[1] += 1
+            elif '-02-' in item[0]:
+                returnDict[2] += 1
+            elif '-03-' in item[0]:
+                returnDict[3] += 1
+            elif '-04-' in item[0]:
+                returnDict[4] += 1
+            elif '-05-' in item[0]:
+                returnDict[5] += 1
+            elif '-06-' in item[0]:
+                returnDict[6] += 1
+            elif '-07-' in item[0]:
+                returnDict[7] += 1
+            elif '-08-' in item[0]:
+                returnDict[8] += 1
+            elif '-09-' in item[0]:
+                returnDict[9] += 1
+            elif '-10-' in item[0]:
+                returnDict[10] += 1
+            elif '-11-' in item[0]:
+                returnDict[11] += 1
+            elif '-12-' in item[0]:
+                returnDict[12] += 1
+
+        print(returnDict)
+        return returnDict
+
 
 
     def executeSql(self, sql, argumentTuple = None):
